989b135928386d631dc26968285f160b08b87f00
md-hasan-ali <hasanaliringku@gmail.com>
2021-09-13 14:28:48 +0600

    added rating and search button design

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
+    console.log(product)
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
@@ -19,6 +20,7 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p>Ratings : ${product.rating.rate} / Total-Rating: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
