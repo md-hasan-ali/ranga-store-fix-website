37d82443154952ad115ba9a1a3f24cc78a12b4e4
md-hasan-ali <hasanaliringku@gmail.com>
2021-09-13 06:18:11 +0600

    some bug fixed

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    console.log(product)
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
